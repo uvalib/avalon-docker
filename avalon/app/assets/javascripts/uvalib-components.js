efb7cabd0c7450c4ec4910bb7735518aa23eca33
Doug Chestnut <dougchestnut@gmail.com>
2021-11-11 13:12:56 -0500

    adding in component modules from cdn and update to footer

---
+++ avalon/app/assets/javascripts/uvalib-components.js
@@ -0,0 +1,12 @@
+let version = "0.0.0";
+let baseUrl = `https://unpkg.internal.lib.virginia.edu/v${version}/`;
+let loadModule = (url)=>{
+        let script = document.createElement('script');
+        script.setAttribute('type','module');
+        script.setAttribute('src',`${baseUrl}${url}`);
+        document.head.appendChild(script);
+    };
+
+loadModule('uvalib-analytics.js');
+loadModule('uvalib-header.js');
+loadModule('uvalib-footer.js');
