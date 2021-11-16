67b74ca98936b5c31692139b5a70ce657fee3d11
Doug Chestnut <dougchestnut@gmail.com>
2021-11-16 16:13:09 -0500

    incrementally load modules

--- avalon/app/assets/javascripts/uvalib-components.js
+++ avalon/app/assets/javascripts/uvalib-components.js
@@ -1,12 +1,18 @@
 let version = "0.0.2";
 let baseUrl = `https://unpkg.internal.lib.virginia.edu/v${version}/`;
-let loadModule = (url)=>{
-        let script = document.createElement('script');
-        script.setAttribute('type','module');
-        script.setAttribute('src',`${baseUrl}${url}`);
-        document.head.appendChild(script);
-    };
+let loadModule = (url)=> new Promise((resolve)=>{
+    let script = document.createElement('script');
+    script.type = 'module';
+    script.onload = resolve;
+    script.src = `${baseUrl}${url}`;
+    document.head.appendChild(script);
+});
 
-loadModule('uvalib-analytics.js');
-loadModule('uvalib-header.js');
-loadModule('uvalib-footer.js');
+// Load up the header first as it most likely is holding up viewable rendering
+loadModule('uvalib-header.js').then(()=>{
+    // Then go ahead and load up the footer so it will be there when they scroll down
+    loadModule('uvalib-footer.js').then(()=>{
+        // With those visible things out of the way, go ahead and load up the analytics tracking
+        loadModule('uvalib-analytics.js');
+    });
+});
