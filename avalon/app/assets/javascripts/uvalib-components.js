d7b74f68b5b3c4f8a62aa3de45fa319807dece62
Doug Chestnut <dougchestnut@gmail.com>
2021-11-18 15:28:44 -0500

    tracking status and affiliation as variables/dimensions

--- avalon/app/assets/javascripts/uvalib-components.js
+++ avalon/app/assets/javascripts/uvalib-components.js
@@ -1,4 +1,4 @@
-let version = "0.0.2";
+let version = "0.0.4";
 let baseUrl = `https://unpkg.internal.lib.virginia.edu/v${version}/`;
 let loadModule = (url)=> new Promise((resolve)=>{
     let script = document.createElement('script');
@@ -13,6 +13,14 @@ loadModule('uvalib-header.js').then(()=>{
     // Then go ahead and load up the footer so it will be there when they scroll down
     loadModule('uvalib-footer.js').then(()=>{
         // With those visible things out of the way, go ahead and load up the analytics tracking
+        // 1 is the id for Status (logged in or not)
+        // 2 is the id for Affiliation (virginia.edu)
+        let status = document.querySelector('.log-in-out').textContent.includes('Sign out');
+        let affiliation = status? document.querySelector('.log-in-out').textContent.replace(/.*\@(.*) \|.*/,'$1').trim(): "none";
+        document.querySelector('uvalib-analytics').setAttribute('variables',JSON.stringify({
+            '1': status? "authenticated":"none",
+            '2': affiliation
+        }));
         loadModule('uvalib-analytics.js');
     });
 });
