aaff8c7444f82d275da4ea94470cbd1b5be9e0e2
Nestor Walker <naw4t@virginia.edu>
2022-08-31 15:09:30 -0400

    exclude path from host check

--- avalon_uva/config/environments/production.rb
+++ avalon_uva/config/environments/production.rb
@@ -131,5 +131,4 @@ Rails.application.configure do
   # Additional production specific initializers
   Dir["config/environments/production/*.rb"].each {|file| load file }
 
-  Rails.application.config.hosts.clear
 end
