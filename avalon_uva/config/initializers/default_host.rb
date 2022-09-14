80c317c634b649033d571f4507c8c94e5371d30d
Nestor Walker <naw4t@virginia.edu>
2022-09-14 13:41:59 -0400

    fix and cleanup host check

---
+++ avalon_uva/config/initializers/default_host.rb
@@ -0,0 +1,24 @@
+server_options = Settings.domain
+server_options = case server_options
+when String
+  uri = URI.parse(server_options)
+  { host: uri.host, port: uri.port, protocol: uri.scheme }
+when Hash
+  server_options
+else
+  server_options.to_hash
+end
+
+if server_options
+  server_options.symbolize_keys!
+  server_options.slice!(:host, :port, :protocol)
+
+  Rails.application.config.to_prepare do
+    Rails.application.routes.default_url_options.merge!( server_options )
+    ActionMailer::Base.default_url_options.merge!( server_options )
+    ApplicationController.default_url_options = server_options
+  end
+
+  Rails.application.config.hosts.clear
+
+end
