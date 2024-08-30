ea25652120c2349914af2253e9364cd32caede0d
Michael Durbin <md5wz@virginia.edu>
2024-08-30 10:04:30 -0400

    Removed broken log statements introduced in the last commit.

--- avalon_uva/app/jobs/ingest_batch_entry_job.rb
+++ avalon_uva/app/jobs/ingest_batch_entry_job.rb
@@ -78,9 +78,6 @@ class IngestBatchEntryJob < ActiveJob::Base
     # @param [BatchEntries] the entry to update
     # @param [Avalon::Batch::Entry] the entry to update
     def invalid_error(batch_entry, entry)
-      puts "Error: #{error.message}"
-      puts "Stack Trace:"
-      puts error.backtrace.join("\n")
       batch_entry.error = true
       batch_entry.complete = false
       batch_entry.current_status = 'Invalid Entry'
