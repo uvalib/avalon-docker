cfbd5f4bb0d31cfc1820a54fe799b3eae6a363b9
Nestor Walker <naw4t@virginia.edu>
2024-10-14 10:05:57 -0400

    Fix post attributes

--- avalon_uva/app/models/glacier_request.rb
+++ avalon_uva/app/models/glacier_request.rb
@@ -7,7 +7,7 @@ class GlacierRequest
   validate :check_bucket_location
 
   def send_request
-    response = RestClient.post(ENV['GLACIER_REQUEST_URL'], nil, query: request_payload.to_json)
+    response = RestClient.post(ENV['GLACIER_REQUEST_URL'], request_payload.to_json)
     puts response.inspect
     # success
 
