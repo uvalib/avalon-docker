a25dc8be3bd39073e964149c773de2e52661714e
Ray Lubinsky <rwl@virginia.edu>
2026-01-08 17:03:29 -0500

    Additional avalon_uva changes to reduce warning messages.

---
+++ avalon_uva/config/initializers/workflow_steps.rb
@@ -0,0 +1,6 @@
+Rails.application.config.to_prepare do
+  HYDRANT_STEPS ||= Workflow.new(FileUploadStep.new,
+                                 ResourceDescriptionStep.new,
+                                 StructureStep.new,
+                                 AccessControlStep.new)
+end
