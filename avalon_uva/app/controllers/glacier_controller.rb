462addafeaa57d971790823ca46359b116be7474
Nestor Walker <naw4t@virginia.edu>
2026-02-04 11:33:56 -0500

    refactor glacier request js

--- avalon_uva/app/controllers/glacier_controller.rb
+++ avalon_uva/app/controllers/glacier_controller.rb
@@ -4,18 +4,17 @@ class GlacierController < ApplicationController
   before_action :set_master_file
 
   def request_masterfile
-
     authorize! :update, @master_file, message: 'You do not have sufficient privileges'
 
-    if current_user && current_ability.is_administrator?
-      gr = GlacierRequest.new(master_file: @master_file, email: current_user.email)
-      gr.send_request if gr.valid?
-    else
-      render json: gr, status: :unauthorized
-      return
+    gr = GlacierRequest.new(master_file: @master_file, email: current_user&.email)
+
+    unless current_user && current_ability.is_administrator?
+      gr.errors.add(:base, 'You do not have sufficient privileges')
+      return render json: gr, status: :unauthorized
     end
 
-    return render json: gr, status: :created
+    gr.send_request if gr.valid?
+
     if gr.errors.present?
       render json: gr, status: :unprocessable_entity
     else
